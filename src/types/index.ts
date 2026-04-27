dd4819ebf22a16bb7a2f11d2b5c2550e39d31757
AbhinavManda-bit <abhimanda2911@gmail.com>
2026-04-27 00:32:18 -0400

    updated locations to be the 3 we agreed on instead of 5

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export type Location = "📊 Van Munching" | "📚 McKeldin Library" | "🎶 Clarice" | "🍽️ STAMP" | "💻 Iribe"; 
+export type Location = "Van Munching" | "McKeldin" | "Clarice";
 
 export interface User {
   id: string;
